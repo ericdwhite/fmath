2b4623ca23a3317547a87a2dce0618da55854d57
Eric D. White <eric@ericwhite.ca>
2011-04-29 22:30:59 +0100

    Basic request logging using Jetty classes, and configure IP/PORT (See: APP_IP, APP_PORT environment variables)

--- clj/src/fmath/core.clj
+++ clj/src/fmath/core.clj
@@ -2,8 +2,45 @@
   (:use fmath.server,
         fmath.randwalk,
         fmath.bigote)
+  (:import (org.mortbay.jetty.handler AbstractHandler)
+           (org.mortbay.jetty.handler RequestLogHandler)
+           (org.mortbay.jetty NCSARequestLog))
   (:gen-class))
 
+(def production?
+  (= "production" (get (System/getenv) "APP_ENV")))
+
+(def development?
+  (not production?))
+
+(defn app-ip []
+  (get (System/getenv) "APP_IP" "127.0.0.1"))
+
+(defn app-port []
+  (Integer/parseInt (get (System/getenv) "APP_PORT" "8080")))
+
+(defn- request-log-hander []
+  "See: 
+    http://jetty.codehaus.org/jetty/jetty-6/apidocs/org/mortbay/jetty/NCSARequestLog.html"
+  (let [log (new NCSARequestLog "jetty-yyyy_mm_dd.request.log")
+        handler (new RequestLogHandler)]
+    (doto log
+      (.setRetainDays 90)
+      (.setAppend true)
+      (.setExtended false)
+      (.setLogLatency true)
+      (.setLogTimeZone "UTC"))
+    (.setRequestLog handler log)
+    handler))
+
+(defn- log-configurator [server]
+  (.addHandler server (request-log-hander)))
+
+(defn server [join configurator]
+  (run-server {:configurator configurator
+               :host (app-ip)
+               :port (app-port)
+               :join? join}))
+
 (defn -main [& args]
-  (run-server {:host "127.0.0.1",
-               :port 8080}))
+  (server true log-configurator))
